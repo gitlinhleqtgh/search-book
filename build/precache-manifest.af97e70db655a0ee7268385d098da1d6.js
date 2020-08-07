1d67aed212a28e91071549cd645036d0636b5015
gitlinhleqtgh <linhleqtgh@gmail.com>
2020-08-07 08:03:38 +0700

    deploy heroku

---
+++ build/precache-manifest.af97e70db655a0ee7268385d098da1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b069c06c96171d11cebac63fd3588e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bfef2cbaa79cb6f352e",
+    "url": "/static/css/main.0ec084e2.chunk.css"
+  },
+  {
+    "revision": "883d3c2d7875f55c5cfb",
+    "url": "/static/js/2.c6347fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6347fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bfef2cbaa79cb6f352e",
+    "url": "/static/js/main.05d768ab.chunk.js"
+  },
+  {
+    "revision": "b60377d80e94edd32657",
+    "url": "/static/js/runtime-main.3dbf1519.js"
+  }
+]);
